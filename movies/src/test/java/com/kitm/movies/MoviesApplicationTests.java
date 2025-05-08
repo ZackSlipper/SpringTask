96e5699d613a4da2a1250d1a10e258e2ca84f317
Swippy <58222035+ZackSlipper@users.noreply.github.com>
2025-05-08 17:48:30 +0300

    Initial commit with sql setup commands

---
+++ movies/src/test/java/com/kitm/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitm.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
